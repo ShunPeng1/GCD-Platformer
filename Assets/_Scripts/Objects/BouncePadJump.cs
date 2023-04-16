b585431e86574247441c6025f75dab9015cbefb9
TruongTamAn2004 <truongtaman2004@gmail.com>
2023-04-16 13:53:12 +0700

    Update: Jump Pad

--- Assets/_Scripts/Objects/BouncePadJump.cs
+++ Assets/_Scripts/Objects/BouncePadJump.cs
@@ -16,7 +16,7 @@ public class BouncePadJump : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag(captainFeet)) {
-            Debug.Log("Found!");
+            
         }
     }
 }
