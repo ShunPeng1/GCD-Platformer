cd5ae8b97a0defa0ace92fce0fdc7b8da2e4caba
ShunPeng1 <shunpeng1@gmail.com>
2023-04-22 08:48:24 +0700

    feat: moving platform attach with player

---
+++ Assets/_Scripts/Interactive Objects/ActivationBar.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Scripts.Interactive_Objects;
+using DG.Tweening;
+using UnityEngine;
+using Sequence = DG.Tweening.Sequence;
+
+
+/*
+[RequireComponent(typeof(Rigidbody2D))]
+public class ActivationBar : MonoBehaviour
+{
+    public ActivationColor BarActivationColor;
+    [SerializeField] private Vector3 _inactivePosition, _activePosition;
+    [SerializeField] private float _movingDuration=  1f;
+    [SerializeField] private Ease _ease = Ease.OutCubic;
+    
+    private Rigidbody2D _rigidbody2D;
+    private Sequence _sequence;
+    private Vector3 _destinationPosition;
+    
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _destinationPosition = transform.position;
+    }
+    
+
+    public void ActiveMove()
+    {
+        _sequence.Kill();
+        _sequence = DOTween.Sequence();
+
+        _destinationPosition += _activePosition;
+        _sequence.Append( 
+            _rigidbody2D.DOMove(_destinationPosition, _movingDuration).SetEase(_ease)
+            );
+        
+        _sequence.Play();
+    }
+
+
+    public void InactiveMove()
+    {
+        _sequence.Kill();
+        _sequence = DOTween.Sequence();
+        
+        _destinationPosition += _inactivePosition;
+        _sequence.Append( 
+            _rigidbody2D.DOMove(_destinationPosition, _movingDuration).SetEase(_ease)
+        );
+        _sequence.Play();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(_inactivePosition + transform.position, "Inactive Position", false, Color.red);
+        Gizmos.DrawIcon(_activePosition + transform.position, "Active Position", false, Color.green);
+    }
+
+}*/
+
+
+public class ActivationBar : MonoBehaviour
+{
+    public ActivationColor BarActivationColor;
+    [SerializeField] private Vector3 _inactivePosition, _activePosition;
+    [SerializeField] private float _movingDuration=  1f;
+    [SerializeField] private Ease _ease = Ease.OutCubic;
+    [SerializeField] private LayerMask _attachLayerMask;
+    
+    private Sequence _sequence;
+    private Vector3 _destinationPosition;
+    
+    
+    private void Awake()
+    {
+        _destinationPosition = transform.position;
+    }
+    
+
+    public void ActiveMove()
+    {
+        _sequence.Kill();
+        _sequence = DOTween.Sequence();
+
+        _destinationPosition += _activePosition;
+        _sequence.Append( 
+            transform.DOMove(_destinationPosition, _movingDuration).SetEase(_ease)
+        );
+        
+        _sequence.Play();
+    }
+
+
+    public void InactiveMove()
+    {
+        _sequence.Kill();
+        _sequence = DOTween.Sequence();
+        
+        _destinationPosition += _inactivePosition;
+        _sequence.Append( 
+            transform.DOMove(_destinationPosition, _movingDuration).SetEase(_ease)
+        );
+        _sequence.Play();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (((1 << col.gameObject.layer) & _attachLayerMask) != 0)
+        {
+            col.transform.SetParent(transform);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (((1 << col.gameObject.layer) & _attachLayerMask) != 0)
+        {
+            col.transform.SetParent(null);
+        }
+    }
+    
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(_inactivePosition + transform.position, "Inactive Position", false, Color.red);
+        Gizmos.DrawIcon(_activePosition + transform.position, "Active Position", false, Color.green);
+    }
+
+}
